3af6efcad6e028802b2479ac2597596a30046ca3
Ibrahim atef eltahlawy <ibrahimatefelttahlawy@gmail.com>
2023-04-20 23:36:15 +0200

    add week4 tasks

---
+++ Week4/LibraryManagementSystem/src/Library.java
@@ -0,0 +1,117 @@
+package Week4.LibraryManagementSystem.src;
+
+import java.util.ArrayList;
+
+public class Library {
+    public String name;
+    public String code;
+    public ArrayList<Book> books;
+    public ArrayList<User> users;
+
+    public Library(String name, String code) {
+        this.name = name;
+        this.code = code;
+        this.books = new ArrayList<>();
+        this.users = new ArrayList<>();
+    }
+
+    // method to add a book to the library
+    public void addBook(Book book){
+        books.add(book);
+    }
+
+    // method to remove a book from the library
+    public void removeBook(Book book) {
+        if (books.contains(book)) {
+            books.remove(book);
+            System.out.println(book.title + " has been removed from the library.");
+        } else {
+            System.out.println(book.title + " is not in the library.");
+        }
+    }
+
+    // method to check out a book in the library
+    public User checkoutBook(User user, int book_id) {
+        for (Book book : books) {
+            if (book.bookID == book_id && book.isAvailable) {
+                user.borrowedBooks.add(book);
+                book.isAvailable = false;
+                System.out.println(user.userName + " has checked out " + book.title + ".");
+                return user;
+            }
+        }
+        System.out.println("Invalid Book ID or Book is not available.");
+        return null;
+    }
+
+    // A method to return a book.
+    public void returnBook(User user, int book_id){
+        for (Book book : user.borrowedBooks){
+            if (book.bookID == book_id && !book.isAvailable){
+                user.borrowedBooks.remove(book);
+                book.isAvailable = true;
+                System.out.println(user.userName + " has returned " + book.title + ".");
+                return;
+            }
+        }
+        System.out.println("Invalid Book ID or Book is not borrowed by the user.");
+    }
+
+    // A method to display the list of available books.
+    public void displayAvailableBooks(){
+        ArrayList<Book> availableBooks = new ArrayList<>();
+        for (Book book : books){
+            if (book.isAvailable){
+                availableBooks.add(book);
+            }
+        }
+        if (availableBooks.size() > 0) {
+            System.out.println("Available books:");
+            for (Book book : availableBooks) {
+                System.out.println(book);
+            }
+        } else {
+            System.out.println("No books are currently available.");
+        }
+    }
+
+    // A method to display the list of borrowed books
+    public void displayBorrowedBooks(){
+        ArrayList<Book> borrowedBooks = new ArrayList<>();
+        for (Book book : books) {
+            if (!book.isAvailable) {
+                borrowedBooks.add(book);
+            }
+        }
+        if (borrowedBooks.size() > 0) {
+            System.out.println("Borrowed books:");
+            for (Book book : borrowedBooks) {
+                System.out.println(book);
+            }
+        } else {
+            System.out.println("No books have been borrowed.");
+        }
+    }
+
+    // A method to display the book details of a specific book.
+    public void displayBookDetails(int book_id){
+        for (Book book : books){
+            if (book.bookID == book_id){
+                System.out.println(book);
+                if (book.isAvailable) {
+                    System.out.println("Status: Available");
+                } else {
+                    for (User u : users) {
+                        if (u.borrowedBooks.contains(book)) {
+                            System.out.println("Status: Borrowed by " + u.userName);
+                            break;
+                        }
+                    }
+                }
+                return;
+            }
+        }
+        System.out.println("Invalid Book ID");
+    }
+
+}
---
+++ Week4/LibraryManagementSystem/src/Main.java
@@ -0,0 +1,127 @@
+package Week4.LibraryManagementSystem.src;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("*** Welcome To My Library Management System App ***");
+        System.out.print("Enter Library Name: ");
+        String libName = input.nextLine();
+        System.out.print("Enter Library Code: ");
+        String libCode = input.nextLine();
+        Library library = new Library(libName, libCode);
+
+        while (true){
+            System.out.println("\nPlease choose your operation from the list!");
+            System.out.println("1- Add book to the library.");
+            System.out.println("2- Remove book from the library.");
+            System.out.println("3- Check out book.");
+            System.out.println("4- Return book.");
+            System.out.println("5- Display book details.");
+            System.out.println("6- Display available books.");
+            System.out.println("7- Display borrowed books.");
+            System.out.println("8- Exit.");
+
+            int operation = input.nextInt();
+            input.nextLine();
+
+            switch (operation){
+                case 1:
+                    System.out.print("\nEnter Book Title: ");
+                    String title = input.nextLine();
+                    System.out.print("Enter Book Author: ");
+                    String author = input.nextLine();
+                    System.out.print("Enter Book ID: ");
+                    int book_id = input.nextInt();
+                    System.out.print("Enter Book Version: ");
+                    int version = input.nextInt();
+                    input.nextLine();
+
+                    Book book = new Book(title, author, book_id, version);
+                    library.addBook(book);
+                    break;
+
+                case 2:
+                    System.out.print("\nEnter Book ID to remove: ");
+                    book_id = input.nextInt();
+                    input.nextLine();
+
+                    for (Book b : library.books){
+                        if (b.bookID == book_id){
+                            library.removeBook(b);
+                            break;
+                        }
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("\nEnter User Name: ");
+                    String userName = input.nextLine();
+                    System.out.print("Enter User ID: ");
+                    int userId = input.nextInt();
+                    input.nextLine();
+
+                    User user = new User(userName, userId);
+
+                    System.out.print("\nEnter Book ID to check out: ");
+                    book_id = input.nextInt();
+                    input.nextLine();
+
+                    User returnedUser = library.checkoutBook(user, book_id);
+                    library.users.add(returnedUser);
+                    break;
+
+                case 4:
+                    User user2 = null;
+
+                    System.out.print("\nEnter user ID: ");
+                    userId = input.nextInt();
+                    input.nextLine();
+
+                    for (User u : library.users) {
+                        if (u.userID == userId) {
+                            user2 = u;
+                            break;
+                        }
+                    }
+
+                    if (user2 == null) {
+                        System.out.println("Invalid User ID.");
+                        break;
+                    }
+
+                    System.out.print("Enter book ID to return: ");
+                    book_id = input.nextInt();
+                    input.nextLine();
+
+                    library.returnBook(user2, book_id);
+                    break;
+                case 5:
+                    System.out.print("\nEnter book ID to display details: ");
+                    book_id = input.nextInt();
+                    input.nextLine();
+
+                    for (Book b : library.books) {
+                        if (b.bookID == book_id) {
+                            library.displayBookDetails(book_id);
+                            break;
+                        }
+                    }
+                    break;
+                case 6:
+                    library.displayAvailableBooks();
+                    break;
+                case 7:
+                    library.displayBorrowedBooks();
+                    break;
+                case 8:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid Number!!, please choose a valid one.");
+                    break;
+            }
+        }
+    }
+}
