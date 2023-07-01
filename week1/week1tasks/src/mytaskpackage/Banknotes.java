6f61ebe63f544e89a666f9760e120f196887ae63
EngAhmedSalah <ahmed.salahfci1164@gmail.com>
2023-07-01 16:11:09 +0200

    tasks added

---
+++ week1/week1tasks/src/mytaskpackage/Banknotes.java
@@ -0,0 +1,27 @@
+package week1.week1tasks.src.mytaskpackage;
+
+import java.util.Scanner;
+
+public class Banknotes {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
+        System.out.println(N);
+        int numOfBanknotes;
+
+        int[] banknotes = {100, 50, 20, 10, 5, 2, 1};
+
+        for (int banknote : banknotes) {
+            if(N < banknote){
+                numOfBanknotes = 0;
+                System.out.println(numOfBanknotes + " nota(s) de R$ " + banknote + ",00");
+            }else{
+                numOfBanknotes = N / banknote;
+                N %= banknote;
+                System.out.println(numOfBanknotes + " nota(s) de R$ " + banknote + ",00");
+            }
+        }
+
+    }
+}
